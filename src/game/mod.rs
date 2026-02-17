c252e7ef6d3fd7775c678fea77aa4fd5a612f21e
jumpiix <david@unterguggenberger.ch>
2026-02-17 14:17:08 +0100

    add game module

---
+++ src/game/mod.rs
@@ -0,0 +1,8 @@
+pub mod player;
+pub mod player_manager;
+pub mod message_handler;
+pub mod spawn_system;
+
+pub use player::Player;
+pub use player_manager::PlayerManager;
+pub use message_handler::MessageHandler;
